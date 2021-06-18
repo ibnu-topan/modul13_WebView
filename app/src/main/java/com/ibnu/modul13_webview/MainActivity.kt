f675c8f8be0cb2ade152fd02cf8ce4d919fa9f95
ibnu-topan <ibnutopanid@gmail.com>
2021-06-19 06:44:59 +0700

    create Web Page

--- app/src/main/java/com/ibnu/modul13_webview/MainActivity.kt
+++ app/src/main/java/com/ibnu/modul13_webview/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.ibnu.modul13_webview
 
-import androidx.appcompat.app.AppCompatActivity
+package tenmb.kotlinandroid
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val message: String = "<html><body><h1>Welcome AndroidKotlin.com</h1></body></html>"
+        val mimeType: String = "text/html"
+        val utfType: String = "UTF-8"
+        webview.loadData(message, mimeType, utfType)
     }
 }
