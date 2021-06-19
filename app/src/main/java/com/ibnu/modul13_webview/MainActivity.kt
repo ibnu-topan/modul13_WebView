ac4fe551122f74e5cfc408afb086e8a5f3625701
ibnu-topan <ibnutopanid@gmail.com>
2021-06-19 10:29:49 +0700

    Add "loadUrl"

--- app/src/main/java/com/ibnu/modul13_webview/MainActivity.kt
+++ app/src/main/java/com/ibnu/modul13_webview/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.ibnu.modul13_webview
 
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val message: String = "<html><body><h1>Welcome AndroidKotlin.com</h1></body></html>"
-        val mimeType: String = "text/html"
-        val utfType: String = "UTF-8"
-        webview.loadData(message, mimeType, utfType)
+        webview.loadUrl("file:///android_asset/web/city.html")
+        webview.settings.javaScriptEnabled = true
     }
 }
