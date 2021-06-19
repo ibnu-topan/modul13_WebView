bbc38c8208a9be57c140cf38d726c142e52db3d4
ibnu-topan <ibnutopanid@gmail.com>
2021-06-19 10:24:14 +0700

    create asset folder

--- app/src/main/java/com/ibnu/modul13_webview/MainActivity.kt
+++ app/src/main/java/com/ibnu/modul13_webview/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.ibnu.modul13_webview
 
-package tenmb.kotlinandroid
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
