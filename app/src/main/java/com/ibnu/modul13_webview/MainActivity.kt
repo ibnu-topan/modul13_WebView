6326d32c995b4dea33427c982c0d182df239a0da
ibnu-topan <ibnutopanid@gmail.com>
2021-06-19 10:42:23 +0700

    Edit to External Web View

--- app/src/main/java/com/ibnu/modul13_webview/MainActivity.kt
+++ app/src/main/java/com/ibnu/modul13_webview/MainActivity.kt
@@ -1,13 +1,44 @@
 package com.ibnu.modul13_webview
 
+import android.app.Activity
+import android.os.Build
 import android.os.Bundle
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        webview.loadUrl("file:///android_asset/web/city.html")
-        webview.settings.javaScriptEnabled = true
+        webview.webViewClient = CustomWebViewClient(this)
+        webview.loadUrl("https://www.google.com/")
+    }
+}
+class CustomWebViewClient internal constructor(private val activity: Activity) :
+    WebViewClient() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun shouldOverrideUrlLoading(
+        view: WebView?,
+        request: WebResourceRequest?
+    ): Boolean {
+        val url: String = request?.url.toString();
+        view?.loadUrl(url)
+        return true
+    }
+    override fun shouldOverrideUrlLoading(webView: WebView, url: String): Boolean {
+        webView.loadUrl(url)
+        return true
+    }
+    override fun onReceivedError(
+        view: WebView,
+        request: WebResourceRequest,
+        error: WebResourceError
+    ) {
+        Toast.makeText(activity, "Error! $error", Toast.LENGTH_SHORT).show()
     }
 }
